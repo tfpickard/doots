8d74dcfc33bba7fee01cd06c9e52feaf141b9935
Tom Pickard <tom@pickard.dev>
2025-03-13 12:36:35 -0600

    Enable plugin and fix alias escape character
    - Enable and configure the `illogical.nvim` plugin for improved functionality in Neovim.
    - Correct the escape character in the printf command within the gcap alias for proper command execution.
    
    [.config/nvim/lua/plugins/illogical.lua]
    - Enable the `illogical.nvim` plugin by uncommenting its entry.
    - Comment out the directory path for the plugin.
    [.aliases]
    - Fix escape character in the printf command within the gcap alias.

--- .config/nvim/lua/plugins/illogical.lua
+++ .config/nvim/lua/plugins/illogical.lua
@@ -1,6 +1,6 @@
 return {
-	-- "tfpickard/illogical.nvim",
-	dir = "~/src/illogical.nvim/",
+	"tfpickard/illogical.nvim",
+	-- dir = "~/src/illogical.nvim/",
 	config = function()
 		require("illogical") -- Ensure the module is loaded
 	end,
