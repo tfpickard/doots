3f947ba639247abe03861de7a2eacfa4f0a7b33f
Tom Pickard <tom@pickard.dev>
2025-04-05 11:25:29 -0600

    chore: Refactor plugin directory determination based on OS
    - Add `.env` file with `OPEN_API_KEY` environment variable
    - Update `ghostty.lua` to set plugin directory dynamically based on the operating system
    - Add `config.toml` file with extensive configuration options for `gptcommit`
    - Add `"notion.nvim"` package with specific branch and commit ID to `lazy-lock.json`

--- .config/nvim/lua/plugins/ghostty.lua
+++ .config/nvim/lua/plugins/ghostty.lua
@@ -3,8 +3,11 @@
 -- know about this in discord
 -- https://discord.com/channels/1005603569187160125/1300462095946485790/1300534513788653630
 
+local is_mac = vim.loop.os_uname().sysname == "Darwin"
+local plugin_dir = is_mac and "/Applications/Ghostty.app/Contents/Resources/vim/vimfiles/" or "/usr/share/nvim/site"
+
 return {
-  "ghostty",
-  dir = "/Applications/Ghostty.app/Contents/Resources/vim/vimfiles/",
-  lazy = false,
+	"ghostty",
+	dir = plugin_dir,
+	lazy = false,
 }
